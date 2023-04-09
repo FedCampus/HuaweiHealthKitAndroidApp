fed1f56dba7e01c92b056d516e31410b90ffc2db
Luyao Wang <lywlywly@gmail.com>
2023-04-10 00:25:23 +0800

    add bottom navigation bar

--- app/src/main/java/com/huaweihealthkitandroidapp/AppScreen.kt
+++ app/src/main/java/com/huaweihealthkitandroidapp/AppScreen.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.sharp.ArrowBack
+import androidx.compose.material.icons.sharp.ArrowForward
+import androidx.compose.material.icons.sharp.Refresh
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.huaweihealthkitandroidapp.ui.AppViewModel
@@ -17,6 +18,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.huaweihealthkitandroidapp.ui.NextNextScreen
 import com.huaweihealthkitandroidapp.ui.NextScreen
 import com.huaweihealthkitandroidapp.ui.SplashScreen
 
@@ -24,6 +26,7 @@ import com.huaweihealthkitandroidapp.ui.SplashScreen
 enum class AppScreen(@StringRes val title: Int) {
   Start(title = R.string.splash_screen),
   Next(title = R.string.app_name),
+  NextNext(title = R.string.password),
 }
 
 @Composable
@@ -50,17 +53,52 @@ fun AppBar(
 }
 
 @Composable
+fun BottomBar(
+  currentScreen: AppScreen,
+  canNavigateBack: Boolean,
+  navigateUp: () -> Unit,
+  navigateMiddle: () -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  BottomNavigation(
+//    title = { Text(stringResource(currentScreen.title)) },
+    modifier = modifier,
+  ) {
+    BottomNavigationItem(
+      selected = true,
+      onClick = navigateUp,
+      icon = {
+        Icon(Icons.Sharp.ArrowBack, contentDescription = null)
+      })
+    BottomNavigationItem(
+      selected = false,
+      onClick = navigateMiddle,
+      icon = { Icon(Icons.Sharp.Refresh, contentDescription = null) })
+    BottomNavigationItem(
+      selected = false,
+      onClick = { /*TODO*/ },
+      icon = { Icon(Icons.Sharp.ArrowForward, contentDescription = null) })
+
+  }
+}
+
+@Composable
 fun App(
   modifier: Modifier = Modifier,
   viewModel: AppViewModel = viewModel(),
-  navController: NavHostController = rememberNavController(),
+//  navController: NavHostController = rememberNavController(),
 ) {
   // Get current back stack entry
+  val navController: NavHostController =
+    rememberNavController() //same effect as pass it in constructor, any changes in a remembered field causes recomposition, whether it is declared inside of in the constructor
   val backStackEntry by navController.currentBackStackEntryAsState()
   // Get the name of the current screen
   val currentScreen = AppScreen.valueOf(
     backStackEntry?.destination?.route ?: AppScreen.Start.name
   )
+
+  var showBottomBar by remember { mutableStateOf(false) }
+
   Scaffold(
     topBar = {
       AppBar(
@@ -69,8 +107,16 @@ fun App(
         navigateUp = { navController.navigateUp() }
       )
     },
-//    bottomBar = {}
-  ) {innerPadding ->
+    bottomBar = {
+      if (showBottomBar)
+        BottomBar(
+          currentScreen = currentScreen,
+          canNavigateBack = navController.previousBackStackEntry != null,
+          navigateUp = { navController.navigateUp() },
+          navigateMiddle = { navController.navigate(AppScreen.NextNext.name) }
+        )
+    }
+  ) { innerPadding ->
     val uiState by viewModel.uiState.collectAsState()
     NavHost(
       navController = navController,
@@ -78,6 +124,7 @@ fun App(
       modifier = modifier.padding(innerPadding)
     ) {
       composable(route = AppScreen.Start.name) {
+        showBottomBar = false
         SplashScreen(
           options = 1,
           onNextButtonClicked = {
@@ -87,12 +134,18 @@ fun App(
         )
       }
       composable(route = AppScreen.Next.name) {
+        showBottomBar = true
         NextScreen(
 //          options = 1,
 //          onNextButtonClicked = {
 //            viewModel.setQuantity(it)
 //            navController.navigate(AppScreen.Next.name)
 //          }
+        )
+      }
+      composable(route = AppScreen.NextNext.name) {
+        showBottomBar = true
+        NextNextScreen(
         )
       }
     }
---
+++ app/src/main/java/com/huaweihealthkitandroidapp/ui/NextNextScreen.kt
@@ -0,0 +1,9 @@
+package com.huaweihealthkitandroidapp.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NextNextScreen() {
+  Text(text = "Sample NextNext Screen")
+}
